4d35e750608d42ae5cfecd5e7b6d3d4001ef0b22
Matt Heath <matt@mattheath.com>
2013-10-10 20:27:50 +0100

    Add Hostpool implementation using Epsilon Greedy algorithm for host selection

--- api/request.go
+++ api/request.go
@@ -19,33 +19,55 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	hostpool "github.com/bitly/go-hostpool"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"runtime"
 	"strings"
+	"sync"
+	"time"
 )
 
-type Request http.Request
+type Request struct {
+	*http.Request
+	hostResponse hostpool.HostPoolResponse
+}
 
 const (
 	Version         = "0.0.1"
 	DefaultProtocol = "http"
 	DefaultDomain   = "localhost"
 	DefaultPort     = "9200"
+
+	// A decay duration of zero results in the default behaviour
+	DefaultDecayDuration = 0
 )
 
 var (
-	_               = log.Ldate
+	_ = log.Ldate
+
+	// Maintain these for backwards compatibility
 	Protocol string = DefaultProtocol
 	Domain   string = DefaultDomain
 	Port     string = DefaultPort
+
+	// Store a slice of hosts in a hostpool
+	Hosts []string
+	hp    hostpool.HostPool
+	once  sync.Once
+
+	// To compute the weighting scores, we perform a weighted average of recent response times,
+	// over the course of `DecayDuration`. DecayDuration may be set to 0 to use the default
+	// value of 5 minutes. The EpsilonValueCalculator uses this to calculate a score
+	// from the weighted average response time.
+	DecayDuration time.Duration = time.Duration(DefaultDecayDuration * time.Second)
 )
 
 type Error struct {
 	Status  int    `json:"status"` // HTTP status code (200, 403, ...)
-	Message string  `json:"error"` // The human readable error message
+	Message string `json:"error"`  // The human readable error message
 }
 
 func (err *Error) Error() string {
@@ -56,14 +78,33 @@ func (err *Error) Error() string {
 	return fmt.Sprintf("Error [%s] Status [%v]", err.Message, err.Status)
 }
 
+// Build HTTP request
+
 func ElasticSearchRequest(method, path string) (*Request, error) {
-	req, err := http.NewRequest(method, fmt.Sprintf("%s://%s:%s%s", Protocol, Domain, Port, path), nil)
+
+	// Setup the hostpool on our first run
+	once.Do(SetupHostpool)
+
+	// Get a host from the Host Pool
+	hr := hp.Get()
+
+	// Get the final host and port
+	host, portNum := splitHostnamePartsFromHost(hr.Host(), Port)
+
+	// Build request
+	req, err := http.NewRequest(method, fmt.Sprintf("%s://%s:%s%s", Protocol, host, portNum, path), nil)
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("User-Agent", "elasticSearch/"+Version+" ("+runtime.GOOS+"-"+runtime.GOARCH+")")
-	return (*Request)(req), nil
+
+	newRequest := &Request{
+		Request:      req,
+		hostResponse: hr,
+	}
+
+	return newRequest, nil
 }
 
 func (r *Request) SetBodyJson(data interface{}) error {
@@ -97,7 +138,11 @@ func (r *Request) SetBody(body io.Reader) {
 }
 
 func (r *Request) Do(v interface{}) (int, []byte, error) {
-	res, err := http.DefaultClient.Do((*http.Request)(r))
+	res, err := http.DefaultClient.Do(r.Request)
+
+	// Inform the HostPool of what happened to the request and allow it to update
+	r.hostResponse.Mark(err)
+
 	if err != nil {
 		return 0, nil, err
 	}
@@ -112,4 +157,38 @@ func (r *Request) Do(v interface{}) (int, []byte, error) {
 		}
 	}
 	return res.StatusCode, bodyBytes, nil
+}
+
+// Set up the host pool to be used
+func SetupHostpool() {
+
+	// If no hosts are set, fallback to defaults
+	if len(Hosts) == 0 {
+		Hosts = append(Hosts, fmt.Sprintf("%s:%s", DefaultDomain, DefaultPort))
+	}
+
+	// Epsilon Greedy is an algorithm that allows HostPool not only to track failure state,
+	// but also to learn about "better" options in terms of speed, and to pick from available hosts
+	// based on how well they perform. This gives a weighted request rate to better
+	// performing hosts, while still distributing requests to all hosts (proportionate to their performance).
+	// The interface is the same as the standard HostPool, but be sure to mark the HostResponse immediately
+	// after executing the request to the host, as that will stop the implicitly running request timer.
+	//
+	// A good overview of Epsilon Greedy is here http://stevehanov.ca/blog/index.php?id=132
+
+	hp = hostpool.NewEpsilonGreedy(Hosts, DecayDuration, &hostpool.LinearEpsilonValueCalculator{})
+
+}
+
+// Split apart the hostname on colon
+// Return the host and a default port if there is no separator
+func splitHostnamePartsFromHost(fullHost string, defaultPortNum string) (string, string) {
+
+	h := strings.Split(fullHost, ":")
+
+	if len(h) == 2 {
+		return h[0], h[1]
+	}
+
+	return h[0], defaultPortNum
 }

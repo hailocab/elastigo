3beb95758832d313019a860ea8462efa656a3ba1
Daniel Cannon <daniel@danielcannon.co.uk>
2015-10-19 10:24:35 +0100

    Add created field to BaseResponse

--- api/baseResponse.go
+++ api/baseResponse.go
@@ -1,11 +1,12 @@
 package api
 
 import (
-  "fmt"
-) 
+	"fmt"
+)
 
 type BaseResponse struct {
 	Ok      bool        `json:"ok"`
+	Created bool        `json:"created"`
 	Index   string      `json:"_index,omitempty"`
 	Type    string      `json:"_type,omitempty"`
 	Id      string      `json:"_id,omitempty"`
@@ -41,21 +42,20 @@ func Pretty(pretty bool) string {
 	return prettyString
 }
 
-
 // http://www.elasticsearch.org/guide/reference/api/search/search-type/
 
 func Scan(scan int) string {
-    scanString := ""
-    if scan > 0 {
-      scanString = fmt.Sprintf("&search_type=scan&size=%v",scan)
-    }
-    return scanString
+	scanString := ""
+	if scan > 0 {
+		scanString = fmt.Sprintf("&search_type=scan&size=%v", scan)
+	}
+	return scanString
 }
 
 func Scroll(duration string) string {
-    scrollString := ""
-    if duration != "" {
-        scrollString = "&scroll="+duration
-    }
-    return scrollString
+	scrollString := ""
+	if duration != "" {
+		scrollString = "&scroll=" + duration
+	}
+	return scrollString
 }
